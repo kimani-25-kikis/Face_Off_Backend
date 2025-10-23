e603229a5211d31dedea15563c1f475f42a2939d
Joshua Ngigi <kimanikikis@gmail.com>
2025-10-23 12:00:12 +0300

    Changed the Email_Pass

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ console.log('Environment variables:', {
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 465,
-  secure: true, // Use TLS
+  secure: true, 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
